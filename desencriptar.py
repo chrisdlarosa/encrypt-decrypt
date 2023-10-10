6693e4d0e13f8d59146f889e40aead8bf7049bcb
Christian De la Rosa <chris.dlarosac@gmail.com>
2023-10-09 23:22:38 -0600

    Update 2

--- desencriptar.py
+++ desencriptar.py
@@ -6,15 +6,15 @@ with open("clave.key", "rb") as key_file:
 cipher_suite = Fernet(key)
 
 # Para recuperar el texto encriptado del archivo más adelante:
-with open("texto_encriptado.txt", "rb") as file:
+with open("mensaje.txt", "rb") as file:
      texto_encriptado = file.read()
 
 texto_desencriptado_bytes = cipher_suite.decrypt(texto_encriptado)
 texto_desencriptado = texto_desencriptado_bytes.decode('utf-8')
 
 # Guarda el texto encriptado en un archivo txt
-with open("texto_desencriptado.txt", "w") as file:
+with open("mensaje.txt", "w") as file:
     file.write(texto_desencriptado)
 
-print("Texto desencriptado y guardado en texto_desencriptado.txt")
+print("Texto desencriptado y guardado en mensaje.txt")
 input()
